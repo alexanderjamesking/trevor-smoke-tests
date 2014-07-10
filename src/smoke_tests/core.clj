ba6bf00640e5be3edc844e6e75019f5a9d05d7e6
Alex King <alex@alexanderjamesking.com>
2014-07-10 17:30:23 +0100

    refactor

--- src/smoke_tests/core.clj
+++ src/smoke_tests/core.clj
@@ -9,24 +9,33 @@
   :throw-exceptions false
 })
 
+(defn call-cps [uri]
+  (let [cps-headers {"X-Candy-Platform" "desktop" "X-Candy-Audience" "domestic" "Accept" "application/json"}
+        res (client/get uri (merge http-conf {:headers cps-headers}))]
+    (json/read-json (:body res))))
+
 (defn call-producer-for-assets [asset-uris]
   (doseq [uri asset-uris]
-    (let [cps-headers {"X-Candy-Platform" "desktop" "X-Candy-Audience" "domestic" "Accept" "application/json"}
-          res (client/get uri (merge http-conf {:headers cps-headers}))
-          body (json/read-json (:body res))
-          asset-uri (-> body :results first :assetUri)]
+    (let [cps-response (call-cps uri)
+          asset-uri (-> cps-response :results first :assetUri)]
       (println asset-uri)
       (try
         (client/get (str "http://127.0.0.1:8185/content/cps" asset-uri))
         (catch Exception e (println "ex1: " (.getMessage e)))))))
 
-(defn get-cps-uris [body] 
-  (for [asset (:results body)]
+(defn get-cps-uris-from-ldp-response [body]
+  (flatten 
+    (for [asset (:results body)]
     (for [locator (:locator asset)]
       (when (.startsWith locator "urn:asset")
-        (str "https://api.live.bbc.co.uk/content/curie/asset:" (.substring locator 10))))))
+        (str "https://api.live.bbc.co.uk/content/curie/asset:" (.substring locator 10)))))))
 
-(defn ldp-uri [concept page]
+(defn call-ldp [uri]
+  (println uri)
+  (let [res (client/get uri (merge http-conf {:headers {"Accept" "application/json-ld"}}))]
+    (json/read-json (:body res))))
+
+(defn build-ldp-uri [concept page]
   (str "https://api.live.bbc.co.uk/ldp-core/creative-works-v2?about=" concept "&format=TextualFormat&page=" page))
 
 (def ldp-concepts '(
@@ -35,7 +44,7 @@
   "2f2db234-3c2d-40a4-b4ac-eea661faadd0" ;business
   "82857f8e-8134-462a-bb32-b7b14f4eab75" ;United States
   "75612fa6-147c-4a43-97fa-fcf70d9cced3" ;politics
-  ; sports...
+  ; sports
   ;"ba6e1118-f874-054e-b159-b797c16e9250"
   ;"ea0aacda-0e5b-4fa2-9f94-dfe775b321a4"
   ;"5cd4682a-7643-f445-8b1f-bcbaf450bc89"
@@ -44,15 +53,10 @@
 ))
 
 (defn -main []
-  (doseq [page (range 1 30)] ; 30 pages of assets
-    (doseq [concept ldp-concepts] ; call each page for each concept
+  (doseq [page (range 1 30)]
+    (doseq [concept ldp-concepts]
       (try
-        (let [ld-uri (ldp-uri concept page)
-              _ (println "Processing: " ld-uri)
-              ; call ldp to get a list of assets for a concept
-              res (client/get ld-uri (merge http-conf {:headers {"Accept" "application/json-ld"}}))
-              body (json/read-json (:body res))
-              ; call cps to get the json for each asset and extract the assetUri
-              asset-uris (remove nil? (flatten (get-cps-uris body)))]
+        (let [ldp-response (call-ldp (build-ldp-uri concept page))
+              asset-uris (remove nil? (get-cps-uris-from-ldp-response ldp-response))]
           (call-producer-for-assets asset-uris))
         (catch Exception e (println "ex0: " (.getMessage e)))))))
